f71c4bd4c03382e891e03c91e8469627d7dfb841
sourav kumar <souravk326@gmail.com>
2022-10-14 13:19:24 +0530

    controller

--- controller/controller.go
+++ controller/controller.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"log"
 
-	""
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
 )
 
 // variable for connect form the MongoDB
@@ -14,14 +18,14 @@ const dbName = "netflix"
 const colName = "watchlist"
 
 //MOST IMPORTANT
-var collection *mongo.collection
+var collection *mongo.Collection
 
 //Connect with mongoDB
 
 func init() {
 	//client option
 
-	clientOption := options.Client().ApplyURL(connectionString)
+	clientOption := options.Client().ApplyURI(connectionString)
 
 	//connect to mongoDB
 	client, err := mongo.Connect(context.TODO(), clientOption)
@@ -35,4 +39,50 @@ func init() {
 
 	//collection instance
 	fmt.Println("Collection instance is ready")
+}
+
+//MONGODB helpers - file
+
+//insert 1 record
+
+func insertOneMovie(movie model.Netflix) { //model is from pacakge model and Netfile is what we create in model file as struct or classs
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted One Movie in database with ID", inserted.InsertedID)
+}
+
+//Update One Movie or record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)       //we used this primitive.ObjectIDFromHex so that it is accssepted my mongodb
+	filter := bson.M{"_id": id}                       //we are find the id in mongodb it can find aoutomatic
+	update := bson.M{"$set": bson.M{"watched": true}} // we are not checking the data we just update the data
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count", result.ModifiedCount)
+}
+
+//delete one Movie or record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie get deleted with deleted count:", deleteCount)
+}
+
+//delete all records from monogdb
+
+func deleteAllMovie() {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.M{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Number of movie deletd is ", deleteResult.DeletedCount)
 }
